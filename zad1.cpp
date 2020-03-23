766013921ff0b4b89e0050baf68b0a3e61615325
Zadanka <62470630+Zadanka@users.noreply.github.com>
2020-03-23 21:48:10 +0100

    zadanie 1

---
+++ zad1.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int a,b,c,m;
+int main()
+{
+    cout << "Podaj trzy liczby oddzielone spacja" << endl;
+    cin >>a>>b>>c;
+    m=a;
+    if (b>m) m=b;
+    if (c>m) m=c;
+    cout << "Wartosc maksymalna: " << m << endl;
+
+
+    return 0;
+}
